f97c3519e77abb3a95827bc2d9f75621e9af8e18
Josh Contreras <thesnarbo@gmail.com>
2024-06-19 15:10:57 +0100

    Dashboard

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ const Navbar = async () => {
                       size: 'sm',
                       variant: 'ghost',
                     })}
-                    href="/api/auth/logout"
+                    href="/dashboard"
                   >
                     Dashboard ✨
                   </Link>
@@ -67,7 +67,7 @@ const Navbar = async () => {
                     size: 'sm',
                     variant: 'ghost',
                   })}
-                  href="/configure/login"
+                  href="/api/auth/login"
                 >
                   Login
                 </Link>
