7236a6a161ebc439f761e28e197d8b39cfda12a1
Josh Contreras <thesnarbo@gmail.com>
2024-06-21 14:53:21 +0100

    Resend update email test

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -63,7 +63,7 @@ export async function POST(req: Request) {
       });
 
       await resend.emails.send({
-        from: 'CaseCobra <josh.r.contreras@gmail.com>',
+        from: 'CaseCobra <onboarding@resend.dev>',
         to: [event.data.object.customer_details.email],
         subject: 'Thanks for your order!',
         react: OrderRecievedEmail({
